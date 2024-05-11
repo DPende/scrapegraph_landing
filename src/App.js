3866fefbd7743658c8670e7f928f931ea2fddb66
DPende <pendesini.daniele@gmail.com>
2024-05-11 16:25:01 +0200

    refactor: moved css files in assets

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //import logo from './logo.svg';
-import './App.css';
+import './assets/css/App.css';
 import Header from './components/Header'
 import Home from './pages/Home'
 import Footer from './components/Footer';
