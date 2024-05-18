3a397a419028547fe6927a55470d7d2942fc207a
DPende <pendesini.daniele@gmail.com>
2024-05-18 15:00:47 +0200

    add: added build script for render.com, which sets render.com environment variables as react global variables (for EmailJs data)

---
+++ scripts/render-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+export REACT_APP_PUBLIC_KEY=$YOUR_PUBLIC_KEY
+export REACT_APP_SERVICE_ID=$YOUR_SERVICE_ID
+export REACT_APP_TEMPLATE_ID=$YOUR_TEMPLATE_ID
+
+npm run build
